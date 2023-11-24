1547499dd5ecdbbdad8377285fd637c2f44e970e
chengshuangfeng <chengshuangfeng@kkworld.com>
2023-11-24 16:29:06 +0800

    feat: 15章案例

--- assets/js/tool.js
+++ assets/js/tool.js
@@ -84,3 +84,77 @@ window.requestAnimationFrame = (
         return window.setTimeout(callback, 1000 / 60);
     }
 )
+
+// 定义边界检测函数 - 边界限制
+const checkBorder = function(cnv, ball) {
+    // 上
+    if ( ball.y < ball.radius) {
+        ball.y = ball.radius;
+    // 下
+    } else if (ball.y > cnv.height - ball.radius) {
+        ball.y = cnv.height - ball.radius;
+    }
+    // 左
+    if (ball.x < ball.radius) {
+        ball.x = ball.radius;
+    // 右
+    } else if (ball.x > cnv.width - ball.radius) {
+        ball.x = cnv.width - ball.radius;
+    }
+}
+
+// 定义边界检测函数 - 边界环绕
+const checkRound = function(cnv, ball) {
+    // 上
+    if ( ball.y < -ball.radius) {
+        ball.y = cnv.height + ball.radius;
+    // 下
+    } else if (ball.y > cnv.height + ball.radius) {
+        ball.y = -ball.radius;
+    }
+    // 左
+    if (ball.x < -ball.radius) {
+        ball.x = cnv.width + ball.radius;
+    // 右
+    } else if (ball.x > cnv.width + ball.radius) {
+        ball.x = -ball.radius;
+    }
+}
+
+const isBeyondBoundary = function(cnv, ball) {
+    return  (ball.x < -ball.radius ||
+    ball.x > cnv.width + ball.radius ||
+    ball.y < -ball.radius ||
+    ball.y > cnv.height + ball.radius)
+}
+
+const BoundaryRebound = function(cnv, ball) {
+    if (ball.x < ball.radius) {
+        ball.x = ball.radius;
+        ball.vx = -ball.vx;
+    } else if (ball.x > cnv.width - ball.radius) {
+        ball.x = cnv.width - ball.radius;
+        ball.vx = -ball.vx;
+    }
+    if (ball.y < ball.radius) {
+        ball.y = ball.radius;
+        ball.vy = -ball.vy;
+    } else if (ball.y > cnv.height - ball.radius) {
+        ball.y = cnv.height - ball.radius;
+        ball.vy = -ball.vy;
+    }
+}
+
+// 获取随机颜色的函数
+const getRandomColor = function() {
+    return '#' + 
+    (function (color) {
+        return (color += '0123456789abcdef'[Math.floor(Math.random() * 16)])
+        && (color.length == 6) ? color : arguments.callee(color);  // 递归调用，直到返回6位颜色值为止。
+    })('');
+}
+
+// 获取随机速度的函数  -1 ~ 1
+const getRandomSpeed = function(speed = 1) {
+    return (Math.random() * 2 - 1) * speed; // -speed ~ speed中间的数值
+}
